a4e3df66792c37a0c765e32c103ef77292014de3
EthanKang1 <ekang@gatech.edu>
2022-12-05 19:18:32 -0500

    Fixed nullv alues in rawpositiondelta

--- main.py
+++ main.py
@@ -59,16 +59,21 @@ def getPosDelta():
 
     newDf = pd.merge(resultsDataset, qualifyingDataset,  how='left', on = ['raceId', 'driverId', 'constructorId'])
 
-    # newDf['qualifyingPosition'].replace(['\\N'], '9999999999999', inplace=True)
-    # newDf['resultPosition'].replace(['\\N'], '-9999999999999', inplace=True)
+    # dataset cleaning
     newDf = newDf[newDf["qualifyingPosition"] != "\\N"]
     newDf = newDf[newDf["resultPosition"] != "\\N"]
+    newDf = newDf[newDf["qualifyingPosition"].notnull()]
+    newDf = newDf[newDf["resultPosition"].notnull()]
 
     newDf["qualifyingPosition"] = pd.to_numeric(newDf["qualifyingPosition"])
     newDf["resultPosition"] = pd.to_numeric(newDf["resultPosition"])
     newDf['PosDelta'] = newDf['resultPosition'].sub(newDf['qualifyingPosition'], axis = 0)
 
     csv2df.outputDf(newDf, "output/rawPositionDelta.csv")
+
+    # group by quali position
+    uniqueQualPositions = newDf['qualifyingPosition'].unique().tolist()
+    print(uniqueQualPositions)
 
 
 getPosDelta()
