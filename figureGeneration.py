d8162621c7f73e35390678cea39423b861d3f564
EthanKang1 <ekang@gatech.edu>
2022-12-13 19:20:26 -0500

    Circuit lap time statistics figure changed x-axis to sort by year

--- figureGeneration.py
+++ figureGeneration.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from utils import csv2df, utilFunc
 import matplotlib.pyplot as plt
+from matplotlib.dates import YearLocator
+import matplotlib.ticker as ticker
 
 def createLapTimeStatsFigures():
     # get datasets
@@ -29,57 +31,28 @@ def createLapTimeStatsFigures():
         IQR = Q3 - Q1
 
         tempDf = tempDf[~((tempDf < (Q1 - 1.5 * IQR)) |(tempDf > (Q3 + 1.5 * IQR))).any(axis=1)]
-        print(tempDf)
+
+        # tempDf.reset_index(inplace=True)
+
+        rotationDf = tempDf.T
+        rotationDf.reset_index(inplace=True)
+
+        rotationDf['year'] = rotationDf["index"].apply(lambda x: racesDataset.get(x)["year"])
+        rotationDf.drop('index', axis=1, inplace=True)
+        print(rotationDf)
+        rotationDf = rotationDf.T
+        print(rotationDf)
+        rotationDf.columns = rotationDf.iloc[-1]
+        rotationDf.drop(rotationDf.index[-1], inplace=True)
+        tempDf = rotationDf
+
+        tempDf = tempDf.reindex(sorted(tempDf.columns), axis=1)
 
         ax = tempDf.plot.box()
+        ax.xaxis.set_major_locator(ticker.AutoLocator())
         plt.show()
         ax.get_figure().savefig('figureOutput/byCircuit/' + uniqueCircuitName + '_lapTimeStats.jpg')
 
-    # aggregate = []
-    # whiskerComp = {}
-    # for index, raceDataset in lapTimesPerRace.items():
-    #     tempDf = racesDataset.loc[racesDataset['raceId'] == index].copy()
-    #     tempDf.drop(['name', 'date', 'time', 'url', 'fp1_date',
-    #                  'fp1_time', 'fp2_date', 'fp2_time', 'fp3_date',
-    #                  'fp3_time', 'quali_date', 'quali_time', 'sprint_date',
-    #                  'sprint_time'], axis=1, inplace=True)
-    #
-    #     raceId = tempDf.iloc[0]['raceId']
-    #     circuitId = tempDf.iloc[0]['circuitId']
-    #
-    #     statDf = raceDataset[["milliseconds"]].describe().T
-    #     statDf.rename(index={'milliseconds': circuitId}, inplace=True)
-    #     statDf.reset_index(inplace=True)
-    #     statDf.rename(columns={'index': 'circuitId'}, inplace=True)
-    #     statDf.insert(0, "raceId", raceId, True)
-    #
-    #     aggregate.append(statDf)
-    #
-    #     whiskerDf = raceDataset[["milliseconds"]].copy()
-    #     whiskerDf.reset_index(inplace=True, drop=True)
-    #     whiskerDf.rename({'milliseconds': raceId}, axis="columns", inplace=True)
-    #
-    #     if circuitId not in whiskerComp.keys():
-    #         whiskerComp[circuitId] = pd.DataFrame()
-    #     whiskerComp[circuitId][raceId] = whiskerDf[raceId]
-    #
-    # aggregateDf = pd.concat(aggregate)
-
-    # uniqueCircuits = circuitsDataset['circuitId'].unique().tolist()
-    #
-    # ## whisker plot
-    # for circuitId in whiskerComp.keys():
-    #     tempDf = whiskerComp[circuitId]
-    #
-    #     Q1 = tempDf.quantile(0.25)
-    #     Q3 = tempDf.quantile(0.75)
-    #     IQR = Q3 - Q1
-    #
-    #     tempDf = tempDf[~((tempDf < (Q1 - 1.5 * IQR)) |(tempDf > (Q3 + 1.5 * IQR))).any(axis=1)]
-    #
-    #     ax = tempDf.plot.box()
-    #     plt.show()
-    #     ax.get_figure().savefig('output/circuitStatsOverTime/' + str(circuitId) + '.jpg')
 
 def correlationFiveOverTime():
     fastestLapSpeedDataset = csv2df.convertDf("regressionOutput/byYear/fastestLapSpeed.csv")
@@ -105,5 +78,5 @@ def correlationFiveOverTime():
 
     plt.show()
 
-correlationFiveOverTime()
+createLapTimeStatsFigures()
 
